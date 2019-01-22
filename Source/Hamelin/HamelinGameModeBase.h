2bdc1492a614961d33b2495b2e1a476813a0c7e6
Noé <neo.laq.33@gmail.com>
2019-01-22 18:33:30 +0100

    Actual initial commit

---
+++ Source/Hamelin/HamelinGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HamelinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAMELIN_API AHamelinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
