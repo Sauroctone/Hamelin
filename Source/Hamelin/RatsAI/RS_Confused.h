3e60c4e5c0c9f4d5b83f036e03035a171003d6c6
Cha <gamijowan@laposte.net>
2019-01-24 22:11:15 +0100

    Created base classes

---
+++ Source/Hamelin/RatsAI/RS_Confused.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RatsAI/RatState.h"
+#include "RS_Confused.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAMELIN_API URS_Confused : public URatState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/Hamelin/RatsAI/RS_Idle.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RatsAI/RatState.h"
+#include "RS_Idle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAMELIN_API URS_Idle : public URatState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
