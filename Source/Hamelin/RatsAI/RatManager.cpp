f8bf5d44cf1955361984617d48d6c518199c3ce9
Cha <gamijowan@laposte.net>
2019-01-26 23:34:23 +0100

    Spawns character (wrong AI controller though)

--- Source/Hamelin/RatsAI/RatManager.cpp
+++ Source/Hamelin/RatsAI/RatManager.cpp
@@ -1,6 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "RatManager.h"
+#include "RatsAI/RatCharacter.h"
+#include "RatsAI/RatAIController.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Engine.h"
 
 
 // Sets default values
@@ -8,13 +12,36 @@ ARatManager::ARatManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+}
 
+void ARatManager::Debug()
+{
+	if (SpawnTarget->IsValidLowLevel())
+	{
+		CreateRat(SpawnTarget);
+		GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Yellow, "RATS !");
+	}
 }
 
 // Called when the game starts or when spawned
 void ARatManager::BeginPlay()
 {
 	Super::BeginPlay();
+	Debug();
+}
+
+void ARatManager::CreateRat(AActor* TargetPoint)
+{
+	FActorSpawnParameters SpawnInfo;
+	ACharacter* RatCharacter = GetWorld()->SpawnActor<ARatCharacter>(TargetPoint->GetActorLocation(), TargetPoint->GetActorRotation(), SpawnInfo);
+	ARatAIController* RatController = NewObject<ARatAIController>(this);
+	RatController->Possess(RatCharacter);
+
+	DanglingRats.Add(RatController);
+}
+
+void ARatManager::DestroyRat(ARatAIController* DestroyedRat)
+{
 	
 }
 
--- Source/Hamelin/RatsAI/RatManager.h
+++ Source/Hamelin/RatsAI/RatManager.h
@@ -4,25 +4,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "RatsAI/RatAIController.h"
 #include "RatManager.generated.h"
+
+
+
 
 UCLASS()
 class HAMELIN_API ARatManager : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
-	// Sets default values for this actor's properties
-	ARatManager();
+	public:	
+		ARatManager();
+		virtual void Tick(float DeltaTime) override;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+		//RAT MANAGEMENT
+		void CreateRat(AActor* TargetPoint); //Spawns a character and an ai controller to possess it
+		void DestroyRat(ARatAIController* DestroyedRat); //Free the memory
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+		//Debug
+		void Debug();
 
-	
-	
+		UPROPERTY(EditAnywhere)
+			AActor* SpawnTarget;
+
+	protected:
+		// Called when the game starts or when spawned
+		virtual void BeginPlay() override;
+		TArray<ARatAIController*> ControlledRats;
+		TArray<ARatAIController*> DanglingRats;
+
 };
